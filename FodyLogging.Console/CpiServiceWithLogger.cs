62213c2e117afb93000086f6bcd4c9e8eb8ec5ff
Steve McKean <steven.mckean@peraton.com>
2025-03-15 09:19:53 -0500

    Added tests for service with logger.

---
+++ FodyLogging.Console/CpiServiceWithLogger.cs
@@ -0,0 +1,28 @@
+﻿using FodyLogging.Console;
+using Microsoft.Extensions.Logging;
+
+[Log(ignorePrivate: false)]
+public class CpiServiceWithLogger
+{
+    private ILogger<CpiService> logger;
+     
+    public CpiServiceWithLogger(ILogger<CpiService> logger)
+    {
+        this.logger = logger;
+        
+    }
+
+    public bool LogReceivedDimensionVariant(CpiSkuDimensionVariant receivedDimensionVariant)
+    {
+        logger.LogInformation("Entering CpiService.LogReceivedDimensionVariant");
+
+        TestMethod();
+        return true;
+    }
+
+    private void TestMethod()
+    {
+        logger.LogInformation("Entering CpiService.TestMethod");
+
+    }
+}
---
+++ LoggingTests/CpiServiceWithLoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FodyLogging.Console;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace LoggingTests
+{
+    public class CpiServiceWithLoggerTests : IClassFixture<LoggerFixture>
+    {
+        private readonly LoggerFixture fixture;
+
+        public CpiServiceWithLoggerTests(LoggerFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void LogReceivedDimensionVariant_ShouldLogCorrectly()
+        {
+            // Arrange
+            var service = new CpiServiceWithLogger(fixture.MockLogger);
+
+            var dimensionVariant = new CpiSkuDimensionVariant
+                {
+                    Sku = "123",
+                    Dimension = "456",
+                    Variant = "789"
+                };
+
+            // Act
+            service.LogReceivedDimensionVariant(dimensionVariant);
+
+            // Assert
+            fixture.MockLogger.Received().Log(
+                LogLevel.Information,
+                Arg.Any<EventId>(),
+                Arg.Is<object>((v) => v.ToString().Contains("Entering CpiService.LogReceivedDimensionVariant")),
+                Arg.Any<Exception>(),
+                Arg.Any<Func<object, Exception, string>>());
+            
+            fixture.MockLogger.Received().Log(
+                LogLevel.Information,
+                Arg.Any<EventId>(),
+                Arg.Is<object>((v) => v.ToString().Contains("Entering CpiService.TestMethod")),
+                Arg.Any<Exception>(),
+                Arg.Any<Func<object, Exception, string>>());
+        }
+    }
+}
